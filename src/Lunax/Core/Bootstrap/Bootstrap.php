6af95b03199bb77d4433d4751afdb4753e627d74
William Cunha <williamoliveira2@hotmail.com>
2018-02-09 16:15:53 -0300

    Added PHP CodeSniffer

--- src/Lunax/Core/Bootstrap/Bootstrap.php
+++ src/Lunax/Core/Bootstrap/Bootstrap.php
@@ -1,9 +1,19 @@
 <?php
+/**
+ * @description Inicialização do projeto
+ */
+
 
 namespace Lunax\Bootstrap;
 
+/**
+ * @description Classe que carrega e inicializa
+ */
 class Bootstrap
 {
+    /**
+     * @description Função encarregada de rodar o projeto
+     */
     public function run()
     {
         return true;
